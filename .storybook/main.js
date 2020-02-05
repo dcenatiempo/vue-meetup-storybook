e13e1db8242ae4189eaec8ab1d9ce18bba1b9c95
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-02-05 16:20:35 -0700

    remove unneeded addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,6 @@ console.log(srcPath)
 
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  addons: [
-    // '@storybook/addon-actions',
-    // '@storybook/addon-links',
-    // '@storybook/addon-knobs/register',
-    // '@storybook/addon-storysource',
-    // '@storybook/addon-docs',
-    // '@storybook/addon-viewport/register',
-    // '@storybook/addon-backgrounds/register',
-  ],
   webpackFinal: async (config, { configType }) => {
     // console.dir(config, { depth: null }) 
     
